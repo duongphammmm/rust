c7a133a1c4a75afb2e818b9c8a52773930c016fb
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-13 20:33:47 +0800

    Finalise guessing-game program function

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -6,15 +7,30 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101); // immutable variable
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new(); // mutable variable:
 
-    let mut guess = String::new(); // mutable variable:
+        io::stdin()
+            .read_line(&mut guess) // references are immutable too, so will need "mut" to make them mutable
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess) // references are immutable too, so will need "mut" to make them mutable
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
 }
--- guessing_game/target/debug/guessing_name.d
+++ guessing_game/target/debug/guessing_name.d
@@ -1,1 +1,1 @@
-C:\Users\Duong\ Pham\Desktop\rust\guessing_name\target\debug\guessing_name.exe: C:\Users\Duong\ Pham\Desktop\rust\guessing_name\src\main.rs
+C:\Users\Duong\ Pham\Desktop\rust\guessing_game\target\debug\guessing_name.exe: C:\Users\Duong\ Pham\Desktop\rust\guessing_game\src\main.rs
