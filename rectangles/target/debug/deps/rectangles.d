977ddbfc5760ab448d59c2fd7dabf14375560c7b
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-14 18:44:46 +0800

    Create rectangles program using struct

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\Users\Duong Pham\Desktop\rust\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+C:\Users\Duong Pham\Desktop\rust\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\Duong\ Pham\Desktop\rust\rectangles\target\debug\rectangles.exe: C:\Users\Duong\ Pham\Desktop\rust\rectangles\src\main.rs
