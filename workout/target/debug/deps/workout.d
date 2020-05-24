d2d42f6064823f04fa140014558ff608fa895060
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-24 23:04:03 +0800

    Create work out program: Closures

---
+++ workout/target/debug/deps/workout.d
@@ -0,0 +1,5 @@
+C:\Users\Duong Pham\Documents\mystuff\codes\rust\workout\target\debug\deps\workout.exe: src\main.rs
+
+C:\Users\Duong Pham\Documents\mystuff\codes\rust\workout\target\debug\deps\workout.d: src\main.rs
+
+src\main.rs:
---
+++ workout/target/debug/workout.d
@@ -0,0 +1,1 @@
+C:\Users\Duong\ Pham\Documents\mystuff\codes\rust\workout\target\debug\workout.exe: C:\Users\Duong\ Pham\Documents\mystuff\codes\rust\workout\src\main.rs
