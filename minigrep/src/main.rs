a388a04001133cbd52f169691725cde5503b7f12
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-24 18:51:01 +0800

    Minigrep: refactor functions in main and improve error-handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,34 @@
 use std::env;
+use std::process;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {} in file {}", query, filename);
+    println!("Searching for {} in file {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Problem reading file");
+    let contents = fs::read_to_string(config.filename)
+        .expect("Problem reading file"); // return a Result<String>
 
     println!("With text\n{}", contents);
+}
+
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+impl Config<'_> {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = &args[1];
+        let filename = &args[2];
+        Ok(Config { query, filename })
+    }
 }
