fdc88e7f667811659d32ff074c0d2a4cd2107a5f
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-24 18:24:27 +0800

    Minigrep: reading arguments from command line

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\Duong Pham\Documents\mystuff\codes\rust\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\Duong Pham\Documents\mystuff\codes\rust\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\Duong\ Pham\Documents\mystuff\codes\rust\minigrep\target\debug\minigrep.exe: C:\Users\Duong\ Pham\Documents\mystuff\codes\rust\minigrep\src\main.rs
