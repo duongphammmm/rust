b4e2cf6adce2f77f2cba9933fc9f9b2cec6aadf8
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-24 15:45:43 +0800

    Unit test: test private function

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,16 +1,9 @@
-pub struct Guess {
-    value: i32,
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
 }
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!("Guess value must be greater than or equal to 1, got {}", value);
-        } else if value > 100 {
-            panic!("Guess value must be smaller than or equal to 100, got {}", value);
-        }
-        Guess { value }
-    }
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -18,8 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic(expected = "Guess value must be smaller than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
