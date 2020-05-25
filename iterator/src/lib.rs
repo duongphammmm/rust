5d019e8cd406cefea3146f68a83365e3286bf565
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-25 11:23:03 +0800

    Implement Iterators

--- iterator/src/lib.rs
+++ iterator/src/lib.rs
@@ -4,9 +4,36 @@ struct Shoe {
     style: String,
 }
 
+
 fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     // closure captures shoe_size from environment
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+// Counter struct that could iterate from 1 to 5
+struct Counter {
+    count: u32,
+}
+
+// constructor for Counter
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0}
+    }
+}
+
+// Iterator trait for Counter
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -45,5 +72,27 @@ mod tests {
                 },
             ]
         );
+    }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new() // Counter that could iterate from 1 to 5
+            .zip(Counter::new().skip(1)) // zip() produces 4 pairs: (1, 2), (2, 3), (3, 4), (4, 5) (return None instead of (5, None))
+            .map(|(a, b)| a * b) // map() to 2, 6, 12, 20
+            .filter(|x| x % 3 == 0) // filter() to 6 and 12 which are divisible by 3
+            .sum();
+
+        assert_eq!(18, sum);
     }
 }
