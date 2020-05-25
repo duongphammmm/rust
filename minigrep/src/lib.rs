50acc3a2a96a5e742bd59699b99745f1d7097350
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-05-25 11:46:13 +0800

    Improve minigrep using iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -18,51 +18,42 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub struct Config<'a> {
-    pub query: &'a str,
-    pub filename: &'a str,
+pub struct Config {
+    pub query: String,
+    pub filename: String,
     pub case_sensitive: bool,
 }
 
-impl Config<'_> {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = &args[1];
-        let filename = &args[2];
+impl Config {
+    // std::env::Args implements the Iterator trait
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); // skip first argument which is the program name
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
         Ok(Config { query, filename, case_sensitive, })
     }
 }
 
-pub fn search<'a>(
-    query: &str,
-    contents: &'a str
-) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+            .filter(|line| line.contains(query))
+            .collect()
 }
 
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+            .filter(|line| line.to_lowercase().contains(&query))
+            .collect()
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,8 +3,8 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // env::args() returns an iterator, not collected yet
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
